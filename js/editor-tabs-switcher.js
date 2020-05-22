056cdd5049529aa19589725fbd1fbfcdbf2149da
kokojek <60446520+kokojek@users.noreply.github.com>
2020-05-22 14:11:57 +0200

    Js
    Jeg kan ikke få js til at fungere, selvom den gjorde det lige før :(

--- js/editor-tabs-switcher.js
+++ js/editor-tabs-switcher.js
@@ -1,23 +1,23 @@
 // JavaScript Document
-function switchedit(editpageswitch){
-			var editpage = ["graphs", "text", "templates", "code"];
-			for(i = 0; i < editpage.length; i++){
-				if(editpageswitch == editpage[i]){
-					document.getElementById(editpage[i]).classList.remove("hidden");
-				}
-				else{
-					document.getElementById(editpage[i]).classList.add("hidden");
-				}
-			}
+function switchedit(switcheditpage){
+	var editpage = ["grpahs", "text", "templates", "code"];
+	for(i = 0; i < editpage.length; i++){
+		if(switcheditpage == editpage[i]){
+			document.getElementById(editpage[i]).classList.remove("hidden");
 		}
-function switcheditdivbg(editbgswitch){
-			var editbg = ["graphsdiv", "textdiv", "templatesdiv", "codediv"];
-			for(i = 0; i < editbg.length; i++){
-				if(editbgswitch == editpage[i]){
-					document.getElementById(editpage[i]).classList.remove("lightbg");
-				}
-				else{
-					document.getElementById(editbg[i]).classList.add("lightbg")
-				}
-			}
+		else{
+			document.getElementById(editpage[i]).classList.add("hidden");
 		}
+	}
+}
+function switcheditbg(editbgswitch){
+	var editbg = ["graphsdiv", "textdiv", "templatesdiv", "codediv"];
+	for(ii = 0; ii < editbg.length; ii++){
+		if(editbgswitch == editbg[i]){
+			document.getElementById(editbg[ii]).classList.remove("lightbg");
+		}
+		else{
+			document.getElementById(editbg[ii]).classList.add("lightbg")
+		}
+	}
+}
