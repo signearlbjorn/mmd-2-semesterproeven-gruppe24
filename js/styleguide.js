6e115f2938ca8d76f10a374566d4d9b3fabbbba1
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2020-05-24 19:01:33 +0200

    Added highchart support. Added section for higchart in styleguide.html. Added some examples.

--- js/styleguide.js
+++ js/styleguide.js
@@ -78,3 +78,107 @@ function dis(element){
 		rowTwo.classList.add("show");
 	}
 }
+
+/* -- Highcharts graphs -- */
+Highcharts.setOptions({
+	colors:  ['#F05440', '#192440', '#F05440', '#337AB7', '#F0AD4E', '#00CD00', '#FF0000'],
+    chart: {
+		backgroundColor: "rgba(0,0,0,0)",
+		borderWidth: 0,
+        plotShadow: false,
+		plotBorderWidth: 0
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+	// #highcharts-type-bar-preview-example
+	var typeBarPreviewExample = Highcharts.chart('highcharts-type-bar-preview-example', {
+		chart: {
+			type: 'bar',
+			animation: false,
+			AnimationOptionsObject: false,
+			backgroundColor: "#FFF",
+			borderWidth: 0,
+			plotShadow: false,
+			plotBorderWidth: 1,
+			plotBorderColor: 0,
+		},
+		tooltip: { enabled: false },
+		legend: { enabled: false },
+		credits: { enabled: false },
+		plotOptions: {
+			series:{
+				states: {
+					inactive: {
+						opacity: 1
+					},
+					hover: {
+						enabled: false
+					}
+				}
+			}
+		},
+		title: {
+			text: 'Bar',
+			align: "left"
+		},
+		xAxis: {
+			title: {
+				text: ''
+			},	
+			categories: ['A', 'B', 'C'],
+		},
+		yAxis: {
+			title: {
+				text: ''
+			},
+		},
+		series: [{
+			name: '1',
+			data: [1, 3, 5]
+			// 0 of caregoy 1
+			// 2 of caregoy 2
+			// 4 of caregoy 3
+		}, {
+			name: '2',
+			data: [2, 4, 6]
+			// 1 of caregoy 1
+			// 3 of caregoy 2
+			// 5 of caregoy 3
+		}]
+	});
+	
+	// #highcharts-type-bar-element-example
+	var typeBarElementExample = Highcharts.chart('highcharts-type-bar-element-example', {
+		chart: {
+			type: 'bar',
+		},
+		title: {
+			text: 'Graph title'
+		},
+		xAxis: {
+			title: {
+				text: 'X-axis title'
+			},	
+			categories: ['Category 1', 'Category 2', 'Category 3']
+		},
+		yAxis: {
+			title: {
+				text: 'Y-axis title'
+			}
+		},
+		series: [{
+			name: 'Series name 1',
+			data: [0, 2, 4]
+			// 0 of caregoy 1
+			// 2 of caregoy 2
+			// 4 of caregoy 3
+		}, {
+			name: 'Series name 2',
+			data: [1, 3, 5]
+			// 1 of caregoy 1
+			// 3 of caregoy 2
+			// 5 of caregoy 3
+		}]
+	});
+});
