5699c7cf9ea7fc42a10ab29fc2f04b4906768ce4
kokojek <60446520+kokojek@users.noreply.github.com>
2020-05-24 11:39:44 +0200

    Placehodlers fjernet

--- js/editor-tabs-switcher.js
+++ js/editor-tabs-switcher.js
@@ -14,12 +14,12 @@ function switchedit(switcheditpage) {
 
 function switcheditbg(editbgswitch) {
 	let editbg = ["graphsDiv", "textDiv", "templatesDiv", "codeDiv"];
-	for(ii = 0; ii < editbg.length; ii++){
-		if(editbgswitch == editbg[ii]){
-			document.getElementById(editbg[ii]).classList.remove("lightbg");
+	for(i = 0; i < editbg.length; i++){
+		if(editbgswitch == editbg[i]){
+			document.getElementById(editbg[i]).classList.remove("lightbg");
 		}
 		else{
-			document.getElementById(editbg[ii]).classList.add("lightbg");
+			document.getElementById(editbg[i]).classList.add("lightbg");
 		}
 	}
 }
@@ -28,15 +28,15 @@ console.log('Init');
 function switchpagepreview(previewSwitch){
 	console.log('Start funktion');
 	let editpreview = ["pageVertical", "pageHorizontal"];
-	for(iii = 0; iii < editpreview.length; iii++){
+	for(i = 0; i < editpreview.length; i++){
 		console.log('loop yai');
-		if(previewSwitch == editpreview[iii]){
+		if(previewSwitch == editpreview[i]){
 			console.log('if');
-			document.getElementById(editpreview[iii]).classList.remove("hidden");
+			document.getElementById(editpreview[i]).classList.remove("hidden");
 		}
 		else{
 			console.log('else');
-			document.getElementById(editpreview[iii]).classList.add("hidden");
+			document.getElementById(editpreview[i]).classList.add("hidden");
 		}
 		console.log('Loop end :-(');
 	}
