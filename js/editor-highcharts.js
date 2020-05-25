8ac65b24d0469e534f9f78c9f11fe35960ae144d
kokojek <60446520+kokojek@users.noreply.github.com>
2020-05-25 13:04:37 +0200

    Editor highcharts FINAL UPDATE

--- js/editor-highcharts.js
+++ js/editor-highcharts.js
@@ -10,78 +10,177 @@ Highcharts.setOptions({
     }
 });
 
-// #highcharts-type-bar-example
 document.addEventListener('DOMContentLoaded', function () {
-    var myChart = Highcharts.chart('highcharts-type-bar-example', {
-        chart: {
-            type: 'bar',
-        },
-        title: {
-            text: 'Graph title'
-        },
-        xAxis: {
-            title: {
-                text: 'X-axis title'
-            },    
-            categories: ['Category 1', 'Category 2', 'Category 3']
-        },
-        yAxis: {
-            title: {
-                text: 'Y-axis title'
+    // Initiate graphs here
+	// #highcharts-type-bar-preview-example
+var typeBarPreviewExample = Highcharts.chart('highcharts-type-bar-preview-example', {
+    chart: {
+        type: 'bar',
+        animation: false,
+        AnimationOptionsObject: false,
+        backgroundColor: "#FFF",
+        borderWidth: 0,
+        plotShadow: false,
+        plotBorderWidth: 1,
+        plotBorderColor: 0,
+    },
+    tooltip: { enabled: false },
+    legend: { enabled: false },
+    credits: { enabled: false },
+    plotOptions: {
+        series:{
+            states: {
+                inactive: {
+                    opacity: 1
+                },
+                hover: {
+                    enabled: false
+                }
             }
-        },
-        series: [{
-            name: 'Series name 1',
-            data: [0, 2, 4]
-            // 0 of caregoy 1
-            // 2 of caregoy 2
-            // 4 of caregoy 3
-        }, {
-            name: 'Series name 2',
-            data: [1, 3, 5]
-            // 1 of caregoy 1
-            // 3 of caregoy 2
-            // 5 of caregoy 3
-        }]
-    });
-
-// #highcharts-type-column-example
-
-    var myChart = Highcharts.chart('highcharts-type-column-example', {
-        chart: {
-            type: 'column',
-        },
+        }
+    },
+    title: {
+        text: 'Bar',
+        align: "left"
+    },
+    xAxis: {
         title: {
-            text: 'Graph title'
+            text: ''
+        },    
+        categories: ['A', 'B', 'C'],
+    },
+    yAxis: {
+        title: {
+            text: ''
         },
-        xAxis: {
-            title: {
-                text: 'X-axis title'
-            },    
-            categories: ['Category 1', 'Category 2', 'Category 3']
-        },
-        yAxis: {
-            title: {
-                text: 'Y-axis title'
-            }
-        },
-        series: [{
-            name: 'Series name 1',
-            data: [0, 2, 4]
-            // 0 of caregoy 1
-            // 2 of caregoy 2
-            // 4 of caregoy 3
-        }, {
-            name: 'Series name 2',
-            data: [1, 3, 5]
-            // 1 of caregoy 1
-            // 3 of caregoy 2
-            // 5 of caregoy 3
-        }]
-    });
+    },
+    series: [{
+        name: '1',
+        data: [1, 3, 5]
+        // 0 of caregoy 1
+        // 2 of caregoy 2
+        // 4 of caregoy 3
+    }, {
+        name: '2',
+        data: [2, 4, 6]
+        // 1 of caregoy 1
+        // 3 of caregoy 2
+        // 5 of caregoy 3
+    }]
 });
-// #highcharts-type-line-example
-
+	// #highcharts-type-column-element-preview    
+var typeColumnPreviewExample = new Highcharts.chart('highcharts-type-column-preview-example', {
+    chart: {
+        type: 'column',
+        animation: false,
+        AnimationOptionsObject: false,
+        backgroundColor: "#FFF",
+        borderWidth: 0,
+        plotShadow: false,
+        plotBorderWidth: 1,
+        plotBorderColor: 0,
+    },
+    tooltip: { enabled: false },
+    legend: { enabled: false },
+    credits: { enabled: false },
+    plotOptions: {
+        series:{
+            states: {
+                inactive: {
+                    opacity: 1
+                },
+                hover: {
+                    enabled: false
+                }
+            }
+        }
+    },
+    title: {
+        text: 'column',
+        align: "left"
+    },
+    xAxis: {
+        title: {
+            text: ''
+        },    
+        categories: ['A', 'B', 'C'],
+    },
+    yAxis: {
+        title: {
+            text: ''
+        },
+    },
+    series: [{
+        name: '1',
+        data: [1, 3, 5]
+        // 0 of caregoy 1
+        // 2 of caregoy 2
+        // 4 of caregoy 3
+    }, {
+        name: '2',
+        data: [2, 4, 6]
+        // 1 of caregoy 1
+        // 3 of caregoy 2
+        // 5 of caregoy 3
+    }]
+});
+	// #highcharts-type-line-element-preview    
+var typelinePreviewExample = new Highcharts.chart('highcharts-type-line-preview-example', {
+    chart: {
+        type: 'line',
+        animation: false,
+        AnimationOptionsObject: false,
+        backgroundColor: "#FFF",
+        borderWidth: 0,
+        plotShadow: false,
+        plotBorderWidth: 1,
+        plotBorderColor: 0,
+    },
+    tooltip: { enabled: false },
+    legend: { enabled: false },
+    credits: { enabled: false },
+    plotOptions: {
+        series:{
+            states: {
+                inactive: {
+                    opacity: 1
+                },
+                hover: {
+                    enabled: false
+                }
+            }
+        }
+    },
+    title: {
+        text: 'line',
+        align: "left"
+    },
+    xAxis: {
+        title: {
+            text: ''
+        },    
+        categories: ['A', 'B', 'C'],
+    },
+    yAxis: {
+        title: {
+            text: ''
+        },
+    },
+    series: [{
+        name: '1',
+        data: [1, 3, 5]
+        // 0 of caregoy 1
+        // 2 of caregoy 2
+        // 4 of caregoy 3
+    }, {
+        name: '2',
+        data: [2, 4, 6]
+        // 1 of caregoy 1
+        // 3 of caregoy 2
+        // 5 of caregoy 3
+    }]
+});
+	document.addEventListener('DOMContentLoaded', function () {
     var myChart = Highcharts.chart('highcharts-type-line-example', {
         chart: {
             type: 'line',
@@ -114,8 +213,9 @@ document.addEventListener('DOMContentLoaded', function () {
             // 5 of caregoy 3
         }]
     });
-
-// #highcharts-type-pie-element-preview    
+});
+	
+})// #highcharts-type-pie-element-preview    
 var typepiePreviewExample = Highcharts.chart('highcharts-type-pie-preview-example', {
     chart: {
         type: 'pie',
